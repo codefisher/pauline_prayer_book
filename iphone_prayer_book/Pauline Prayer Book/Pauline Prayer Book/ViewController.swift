4ad30077c1cfdcd1425cddaac2011588dac2e9f2
Eremita <Biblicum@Eremita-PIB.local>
2017-04-25 21:10:11 +0200

    adding iphone code

--- iphone_prayer_book/Pauline Prayer Book/Pauline Prayer Book/ViewController.swift
+++ iphone_prayer_book/Pauline Prayer Book/Pauline Prayer Book/ViewController.swift
@@ -9,15 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var webview: UIWebView!
+
+    
+    @IBOutlet weak var indexButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("loading")
+        
+        //indexButton.target = self
+        //indexButton.action = #selector(indexAction)
+        
+        let htmlFile = NSBundle.mainBundle().URLForResource("prayers/pl/front", withExtension: "html" )
+        //let html = try? String(contentsOfFile: htmlFile!, encoding: NSUTF8StringEncoding)
+        let url = NSURLRequest(URL: htmlFile!)
+        webview.loadRequest(url)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: Actions
+    
+    @IBAction func indexAction(sender: UIBarButtonItem) {
+  
+        let viewController: ViewController = self.storyboard?.instantiateViewControllerWithIdentifier("indexView") as! ViewController
+        self.presentViewController(viewController, animated: true, completion: nil)
+    }
+    
+    
+
+    @IBAction func menuButton(sender: UIBarButtonItem) {
     }
 
 
