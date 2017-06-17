81fa75a6bd18798684364eee6c8d1adf38cfb14f
codefisher <mail@codefisher.org>
2017-06-17 21:02:31 +0200

    Simplified the music player

--- android_prayer_book/app/src/main/java/au/org/paulinefathers/paulineprayerbook/PrayerActivity.java
+++ android_prayer_book/app/src/main/java/au/org/paulinefathers/paulineprayerbook/PrayerActivity.java
@@ -53,69 +53,13 @@ public class PrayerActivity extends AppCompatActivity implements SharedPreferenc
             mediaPlayer.release();
             mediaPlayer = null;
         }
-        switch (name) {
-            case "monstra-te":
-                mediaPlayer = MediaPlayer.create(this, R.raw.monstra_te);
-                break;
-            case "agnus-die":
-                mediaPlayer = MediaPlayer.create(this, R.raw.agnus_die);
-                break;
-            case "de-profundis":
-                mediaPlayer = MediaPlayer.create(this, R.raw.de_profundis);
-                break;
-            case "kyrie":
-                mediaPlayer = MediaPlayer.create(this, R.raw.kyrie);
-                break;
-            case "o-maria-virgo-honesta":
-                mediaPlayer = MediaPlayer.create(this, R.raw.o_maria_virgo_honesta);
-                break;
-            case "orationes-i":
-                mediaPlayer = MediaPlayer.create(this, R.raw.orationes_i);
-                break;
-            case "orationes-ii":
-                mediaPlayer = MediaPlayer.create(this, R.raw.orationes_ii);
-                break;
-            case "orationes-iii":
-                mediaPlayer = MediaPlayer.create(this, R.raw.orationes_iii);
-                break;
-            case "orationes-iv":
-                mediaPlayer = MediaPlayer.create(this, R.raw.orationes_iv);
-                break;
-            case "orationes-v-i":
-                mediaPlayer = MediaPlayer.create(this, R.raw.orationes_v_i);
-                break;
-            case "orationes-v-ii":
-                mediaPlayer = MediaPlayer.create(this, R.raw.orationes_v_ii);
-                break;
-            case "orationes-vi":
-                mediaPlayer = MediaPlayer.create(this, R.raw.orationes_vi);
-                break;
-            case "salve-regina":
-                mediaPlayer = MediaPlayer.create(this, R.raw.salve_regina);
-                break;
-            case "sub-tuum-praesidium":
-                mediaPlayer = MediaPlayer.create(this, R.raw.sub_tuum_praesidium);
-                break;
-            case "versus-et-responsiones-i":
-                mediaPlayer = MediaPlayer.create(this, R.raw.versus_et_responsiones_i);
-                break;
-            case "versus-et-responsiones-ii":
-                mediaPlayer = MediaPlayer.create(this, R.raw.versus_et_responsiones_ii);
-                break;
-            case "versus-et-responsiones-iii":
-                mediaPlayer = MediaPlayer.create(this, R.raw.versus_et_responsiones_iii);
-                break;
-            case "versus-et-responsiones-iv":
-                mediaPlayer = MediaPlayer.create(this, R.raw.versus_et_responsiones_iv);
-                break;
-            case "versus-et-responsiones-v":
-                mediaPlayer = MediaPlayer.create(this, R.raw.versus_et_responsiones_v);
-                break;
-            default:
-                return;
-        }
-        if(mediaPlayer != null) {
-            mediaPlayer.start();
+        int resourceID = getResources().getIdentifier(name.replace('-', '_'), "raw", getPackageName());
+        if(resourceID != 0) {
+            mediaPlayer = MediaPlayer.create(this, resourceID);
+
+            if (mediaPlayer != null) {
+                mediaPlayer.start();
+            }
         }
     }
 
@@ -266,8 +210,7 @@ public class PrayerActivity extends AppCompatActivity implements SharedPreferenc
                 return true;
             case R.id.action_index:
                 Intent intent = new Intent(this, IndexActivity.class);
-                int requestCode = 109;
-                startActivityForResult(intent, requestCode);
+                startActivityForResult(intent, OPTIONS_REQUEST_CODE);
                 return true;
             case R.id.action_about:
                 WebView webView = (WebView) findViewById(R.id.webView);
