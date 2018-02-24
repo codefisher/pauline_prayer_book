dcccbc771c64d585ddb0b58af1ab354e5a23126a
codefisher <testo.moz@gmail.com>
2018-02-24 15:30:23 +1100

    Adding changes to prayer book texts.

--- android_prayer_book/app/src/main/assets/prayers/convert_html.py
+++ android_prayer_book/app/src/main/assets/prayers/convert_html.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from slugify import slugify
+#from slugify import slugify
 from lxml import html
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 from lxml.etree import Element
 import re
 import codecs
@@ -16,6 +16,22 @@ opts, args = getopt.getopt(sys.argv[1:], "p",
 
 command_opts = dict(opts)
 
+pathroot = command_opts.get("--input", "./src/")
+path_output = command_opts.get("--output", "./")
+
+import unicodedata
+
+def slugify(string):
+    """
+    Taken from the slugify package, but working with Py3
+    """
+
+    return re.sub(r'[-\s]+', '-',
+                  str(re.sub(r'[^\w\s-]', '',
+                             unicodedata.normalize('NFKD', string))
+                          .strip()
+                          .lower()))
+
 class Library(object):
     tags = {}
 
@@ -23,7 +39,7 @@ class Library(object):
     def tag(cls, name=None, func=None):
         cls.tags[name] = func
 
-def format_prayer(node):
+def format_prayer(node, *args, **kwargs):
     text = (node.text if node.text else '') + ''.join(html.tostring(n).decode() for n in node)
     if "lines" in node.attrib:
         items = []
@@ -42,7 +58,7 @@ def format_prayer(node):
 
 Library.tag("//prayer", format_prayer)
 
-def format_player(node):
+def format_player(node, *args, **kwargs):
     name = node.text_content()
     if "mutiple" in node.attrib:
         mutiple = ", " + node.attrib.get("mutiple")
@@ -62,7 +78,7 @@ Library.tag("//player", format_player)
 
 # such a tag might seem useless, but it was needed for
 # the script that converted the html to tex
-def format_music(node):
+def format_music(node, *args, **kwargs):
     name = node.text_content()
     return html.fromstring("""
     <img class="img-content" src="../music/{}.svg" />
@@ -71,11 +87,17 @@ def format_music(node):
 Library.tag("//music", format_music)
 Library.tag("//gregorian", format_music)
 
-def format_head(node):
-    code_nodes = html.fragments_fromstring("""
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link href="../style.css" rel="stylesheet">
-    """)
+def format_head(node, path='', *args, **kwargs):
+    if isfile(os.path.join(path, '../../../style.css')):
+        code_nodes = html.fragments_fromstring("""
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <link href="../../style.css" rel="stylesheet">
+        """)
+    else:
+        code_nodes = html.fragments_fromstring("""
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <link href="../style.css" rel="stylesheet">
+        """)
     for code_node in code_nodes:
         node.append(code_node)
     return node
@@ -83,17 +105,17 @@ def format_head(node):
 Library.tag("//head", format_head)
 
 if "-p" in command_opts:
-    def format_print(node):
+    def format_print(node, *args, **kwargs):
         root = Element("div")
         for n in node:
             root.append(n)
         return root
-    def format_ebook(node):
+    def format_ebook(node, *args, **kwargs):
         return None
 else:
-    def format_print(node):
+    def format_print(node, *args, **kwargs):
         return None
-    def format_ebook(node):
+    def format_ebook(node, *args, **kwargs):
         root = Element("div")
         for n in node:
             root.append(n)
@@ -102,13 +124,16 @@ else:
 Library.tag("//print", format_print)
 Library.tag("//ebook", format_ebook)
 
+Library.tag("//break", lambda x, *args, **kwargs: None)
+
+
 def from_string(text):
     try:
         return html.fragment_fromstring(text)
     except:
         return html.fragment_fromstring(text, create_parent='div')
 
-def format_litany(node):
+def format_litany(node, *args, **kwargs):
     root = Element("div")
     root.attrib["class"] = "litany"
     if "class" in node.attrib:
@@ -116,8 +141,11 @@ def format_litany(node):
     text = (node.text if node.text else '') + ''.join(html.tostring(n).decode() for n in node)
     for i, row in enumerate(text.strip().split("\n")):
         row_node = Element("div")
-        row_node.append(from_string(row))
         row_node.attrib["class"] = "litany_item"
+        if row.strip().startswith('-'):
+            row_node.attrib["class"] = "litany_item indent-psalm-line"
+            row = row.strip().lstrip("-")
+        row_node.append(from_string(row))
         root.append(row_node)
         if (i == 0 and "responce" in node.attrib) or "all" in node.attrib:
             res_node = Element("div")
@@ -129,7 +157,7 @@ def format_litany(node):
 
 Library.tag("//litany", format_litany)
 
-def format_contents(node):
+def format_contents(node, *args, **kwargs):
     root = Element("ul")
     root.attrib["class"] = "table-of-contents"
     headings = node.xpath("//h2")
@@ -150,7 +178,7 @@ def format_contents(node):
 
 Library.tag("//contents", format_contents)
 
-def format_psalm(node):
+def format_psalm(node, *args, **kwargs):
     root = Element("div")
     root.attrib["class"] = "psalm"
     text = (node.text if node.text else '') + ''.join(html.tostring(n).decode() for n in node)
@@ -178,38 +206,77 @@ def format_psalm(node):
 
 Library.tag("//psalm", format_psalm)
 
+def format_sp(node, *args, **kwargs):
+    tail = node.tail if node.tail else ''
+    if node.text == 'R/':
+        return from_string('<span><span class="rubric">R.</span>' + tail + '</span>')
+    if node.text == 'V/':
+        return from_string('<span><span class="rubric">V.</span>' + tail + '</span>')
+    if node.text == '+':
+        return from_string('<span><span class="rubric">&dagger;</span>' + tail + '</span>')
+
+Library.tag("//sp", format_sp)
+
+def format_reading(node, *args, **kwargs):
+    root = Element("div")
+    if not node.text:
+        if "ref" in node.attrib:
+            root.append(from_string("<div class='ref'>{}</div>".format(node.attrib.get("ref"))))
+    if "label" in node.attrib:
+        root.append(from_string("<div class='reading'>{}</div>".format(node.attrib.get("label"))))
+    if node.text:
+        if "ref" in node.attrib:
+            root.append(from_string("<div class='ref'>{}</div>".format(node.attrib.get("ref"))))
+        root.append(from_string("<div>{}</div>".format(node.text)))
+    for n in node:
+        root.append(n)
+    if "shorttag" in node.attrib:
+        root.append(from_string("<div class='rubric'>{}</div>".format(node.attrib.get("shorttag"))))
+    if "longref" in node.attrib:
+        root.append(from_string("<div class='rubric' style='text-align:center'>{}</div>".format(node.attrib.get("longref"))))
+    if "tagline" in node.attrib:
+        root.append(from_string("<div class='rubric' style='text-align:center; font-size:0.8em;'><em>{}</em></div>".format(node.attrib.get("tagline"))))
+    return root
+
+Library.tag("//reading", format_reading)
+
 def extended_html(text, *args, **kwargs):
     nodes = html.fromstring(text)
     for name, func in Library.tags.items():
         for n in nodes.xpath(name):
-            new = func(n)
+            new = func(n, *args, **kwargs)
             if new is not None:
                 n.getparent().replace(n, new)
             else:
                 n.getparent().remove(n)
     return nodes.getroottree()
 
+def update_files(pathroot, path_output):
+    for folder in (f for f in listdir(pathroot) if isdir(join(pathroot, f))):
+        update_files(os.path.join(pathroot, folder), os.path.join(path_output, folder))
+    for filename in (f for f in listdir(pathroot) if isfile(join(pathroot, f))):
+        if filename == "empty.html" or filename.startswith('.'):
+            continue
+        with codecs.open(join(pathroot, filename), "r", encoding='utf8') as fp:
+            try:
+                text = fp.read()
+                text = text.replace("ę", "&#x119;").replace("Ȩ", "&#x228;") # does not display otherwise
+                text = text.replace("...", "&hellip;")
+                text = re.sub("-(\n|\r)\s*", "", text) # copying from PDF gives words broken by -
+            except UnicodeDecodeError as e:
+                print("Can't read: {}".format(filename))
+                exit()
+            try:
+                doc = extended_html(text, path=pathroot, filename=filename)
+            except Exception as e:
+                print(filename)
+                raise  e
+            for node in doc.xpath("//div[contains(@class, 'prayer-group')]"):
+                if len(re.sub("\s+", "", node.text_content())) < 150:
+                    node.classes.add("short-prayer")
+            for node in doc.xpath("//div[contains(@class, 'prayer')]"):
+                if len(re.sub("\s+", "", node.text_content())) < 100:
+                    node.classes.add("short-prayer")
+        doc.write(os.path.join(path_output, filename))
 
-pathroot = command_opts.get("--input", "./src/")
-path_output = command_opts.get("--output", "./")
-
-for filename in (f for f in listdir(pathroot) if isfile(join(pathroot, f))):
-    if filename == "empty.html":
-        continue
-    with codecs.open(join(pathroot, filename), "r", encoding='utf8') as fp:
-        text = fp.read()
-        text = text.replace("ę", "&#x119;").replace("Ȩ", "&#x228;") # does not display otherwise
-        text = text.replace("...", "&hellip;")
-        text = re.sub("-(\n|\r)\s*", "", text) # copying from PDF gives words broken by -
-        try:
-            doc = extended_html(text)
-        except Exception as e:
-            print(filename)
-            raise  e
-        for node in doc.xpath("//div[contains(@class, 'prayer-group')]"):
-            if len(re.sub("\s+", "", node.text_content())) < 150:
-                node.classes.add("short-prayer")
-        for node in doc.xpath("//div[contains(@class, 'prayer')]"):
-            if len(re.sub("\s+", "", node.text_content())) < 100:
-                node.classes.add("short-prayer")
-    doc.write(os.path.join(path_output, filename))
+update_files(pathroot, path_output)
